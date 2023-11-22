69515a05abd36cf47bf64e79a4a749ecee825835
Baranaba Mugabane <bashybaranaba@Baranabas-MacBook-Pro.local>
2023-11-22 19:10:37 +0300

    adding artist page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,23 @@
+import { User } from "@/models/User";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function formatCount(count: number): string {
+  if (count < 1000) {
+    return count.toString(); // Return the count as is if it's less than 1000
+  } else if (count < 1000000) {
+    // For counts in thousands
+    const formattedCount = (count / 1000).toFixed(1);
+    return `${formattedCount}K`;
+  } else {
+    // For counts in millions
+    const formattedCount = (count / 1000000).toFixed(1);
+    return `${formattedCount}M`;
+  }
 }
 
 export function generateName() {
@@ -58,3 +73,9 @@ export function generateName() {
 
   return `${randomAdjective} ${randomNoun}`;
 }
+
+export const getLoggedUser = (): User | null => {
+  const user = localStorage.getItem("user");
+  if (!user) return null;
+  return JSON.parse(user);
+};
--- models/User.ts
+++ models/User.ts
@@ -1,12 +1,24 @@
 import { defaultProfile } from "@/data/defaults";
 import { generateName } from "@/lib/utils";
+import { Collection } from "./Collection";
+import { Community } from "./Community";
+import { TunedModel } from "./TunedModel";
 import mongoose from "mongoose";
 
 export interface User extends mongoose.Document {
   web3Address: string;
   name: string;
+  description: string;
+  tags: string[];
   email: string;
   picture: string;
+  followers: User[];
+  following: User[];
+  communities: Community[];
+  models: TunedModel[];
+  collections: Collection[];
+  works: string[];
+  createdAt: Date;
   credits: number;
   featured: boolean;
 }
@@ -27,13 +39,29 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: generateName(),
     },
+    tags: {
+      type: [String],
+      default: [],
+    },
     picture: {
       type: String,
       default: defaultProfile,
     },
+    followers: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+      default: [],
+    },
+    following: {
+      type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+      default: [],
+    },
     credits: {
       type: Number,
       default: 100,
+    },
+    description: {
+      type: String,
+      default: null,
     },
     featured: {
       type: Boolean,
